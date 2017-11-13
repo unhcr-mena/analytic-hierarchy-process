6fafb7f91cf34ac2210a34ef2b84539ba8e3f120
Edouard-Legoupil <edouard.legoupil@gmail.com>
2017-11-13 15:59:12 +0200

    Revision

--- code/2-build-hierarchy.R
+++ code/2-build-hierarchy.R
@@ -1,17 +1,24 @@
 ### code to build the AHP hierarchy files based on a csv data file from the xlsform questionnaire
+
+## This requires to build a list with the correct elements and then save it as a YAML file
+
 ## install.packages("yaml")
 
-library("yaml")
+library(yaml)
 library(R6)
 library(data.tree)
 library(ahp)
 
 mainDir <- getwd()
-mainDirroot <- substring(mainDir, 0 , nchar(mainDir)- 5)
+
 ahpFile <- paste0(mainDir,"/data/car.ahp")
 #processedAHP <- Load(ahpFile)
 
 ## Reverse existing demo
 
-test <- yaml.load_file(ahpFile)
-test <-yaml.load(ahpFile)
+ahptree <- yaml.load_file(ahpFile)
+
+
+#ahptree$Version
+#ahptree$Alternatives
+#ahptree$Goal
