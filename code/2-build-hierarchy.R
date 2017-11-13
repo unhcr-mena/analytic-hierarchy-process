af263efd7fa86bf7c36f4b81d2d4f3865dd3e343
Edouard-Legoupil <edouard.legoupil@gmail.com>
2017-11-13 10:24:26 +0200

    Rev development of hierarchy

--- code/2-build-hierarchy.R
+++ code/2-build-hierarchy.R
@@ -1,1 +1,17 @@
 ### code to build the AHP hierarchy files based on a csv data file from the xlsform questionnaire
+## install.packages("yaml")
+
+library("yaml")
+library(R6)
+library(data.tree)
+library(ahp)
+
+mainDir <- getwd()
+mainDirroot <- substring(mainDir, 0 , nchar(mainDir)- 5)
+ahpFile <- paste0(mainDir,"/data/car.ahp")
+#processedAHP <- Load(ahpFile)
+
+## Reverse existing demo
+
+test <- yaml.load_file(ahpFile)
+test <-yaml.load(ahpFile)
